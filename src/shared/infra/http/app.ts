85600522f526ee0b2a8928c20f8e6e20d35d9ebf
Marcos Mathias <marcos-github@memathias.com>
2025-08-02 17:47:59 -0300

    chore: added error management

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,10 +1,33 @@
-import express from 'express';
+import express, { Request, Response, NextFunction, Router } from 'express';
 import 'dotenv/config';
+import { errors } from 'celebrate';
+import AppError from '@shared/errors/AppError';
 
 import routes from './routes';
 
 const app = express();
 
+const temp = Router();
+
 app.use(routes);
+app.use(temp);
+
+app.use(errors());
+app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  // eslint-disable-next-line no-console
+  console.error(err);
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 export default app;
